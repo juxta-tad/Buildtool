9ba06b5b3c17fc872eaf7d9a8446d0b9531caf2f
Matt_ <80498348+juxta-tad@users.noreply.github.com>
2025-12-26 01:47:32 +0000

    Update GoogleTest macro parsing in runnables.scm

--- .zed/extensions/cpp-runnables/languages/cpp/runnables.scm
+++ .zed/extensions/cpp-runnables/languages/cpp/runnables.scm
@@ -7,14 +7,13 @@
 
 ; GoogleTest TEST/TEST_F/TEST_P macros
 ; Parsed as function_definition with TEST as the function name and suite/test as parameters
-; @SUITE becomes ZED_CUSTOM_SUITE, @run becomes ZED_SYMBOL
 ((function_definition
     declarator: (function_declarator
         declarator: (identifier) @_macro
         parameters: (parameter_list
             (parameter_declaration
-                type: (type_identifier) @SUITE)
+                type: (type_identifier) @GTEST_SUITE)
             (parameter_declaration
-                type: (type_identifier) @run))))
+                type: (type_identifier) @run @GTEST_NAME))))
   (#match? @_macro "^TEST(_F|_P)?$")
   (#set! tag cpp-gtest))
