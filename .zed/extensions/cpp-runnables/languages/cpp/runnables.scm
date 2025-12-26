79f14c10ad5a840fe0ac28699fd1d41d6877bd3f
Matt_ <80498348+juxta-tad@users.noreply.github.com>
2025-12-26 01:09:22 +0000

    Fix parameter type for GoogleTest macros

--- .zed/extensions/cpp-runnables/languages/cpp/runnables.scm
+++ .zed/extensions/cpp-runnables/languages/cpp/runnables.scm
@@ -7,12 +7,13 @@
 
 ; GoogleTest TEST/TEST_F/TEST_P macros
 ; Parsed as function_definition with TEST as the function name and suite/test as parameters
+; @SUITE becomes ZED_CUSTOM_SUITE, @run becomes ZED_SYMBOL
 ((function_definition
     declarator: (function_declarator
         declarator: (identifier) @_macro
         parameters: (parameter_list
             (parameter_declaration
-                type: (type_identifier) @_suite)
+                type: (type_identifier) @SUITE)
             (parameter_declaration
                 type: (type_identifier) @run))))
   (#match? @_macro "^TEST(_F|_P)?$")
